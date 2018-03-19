460964fc1acb5ca393a2a2a43a9f372a9143c9e2
Daniel <factordany@yahoo.es>
2018-03-19 12:07:11 +0100

    suma

--- 12_js/03_sentencias/js/condiciones.js
+++ 12_js/03_sentencias/js/condiciones.js
@@ -1,35 +1,36 @@
 const userPepe = {
-    nombre: 'Pepe',
-    apellido: 'Perez',
-    edad: 17,
-    aficiones: ['deporte', 'papiroflexia'],
-    isCasado: false,
-    genero: 'M'
-}
+    nombre : 'Pepe',
+    apellido : 'Perez',
+    edad : 17,
+    aficiones : ['deporte', 'papiroflexia'],
+    isCasado : false,
+    genero :  'M'}
+
 const userMaria = {
-    nombre: 'Maria',
-    apellido: 'Perez',
-    edad: 53,
-    aficiones: ['papiroflexia', 'lectura'],
-    isCasado: true,
-    genero: 'F'
-}
-const MAYOREDAD = 18
+    nombre : 'Maria',
+    apellido : 'Lopez',
+    edad : 53,
+    aficiones : ['papiroflexia', 'lectura'],
+    isCasado : true,
+    genero :  'F'
+    }
+
+const MAYORIAEDAD = 18
 
 function crearSaludo(user) {
-    let mensaje = 'Hola '
-    if (user.edad >= MAYOREDAD && user.genero == 'M'){
+    let mensaje = 'Hola ' 
+    if (user.edad >= MAYORIAEDAD && user.genero == 'M') {
         mensaje += 'D. '
-    } else if(user.edad >= MAYOREDAD && user.genero == 'F') {
+    } else if(user.edad >= MAYORIAEDAD && user.genero == 'F') {
         mensaje += 'Dña. '
     }
     mensaje += user.nombre
-    if (user.isCasado) {
+    if ( user.isCasado ) {
         if (user.genero == 'M') {
             mensaje += '. Saludos a tu mujer'
         } else {
             mensaje += '. Saludos a tu marido'
-        }
+        }   
     }
     return mensaje
 }
@@ -38,20 +39,20 @@ function saludar(usuario) {
     console.log(crearSaludo(usuario))
 }
 
-function saludarHTML(usuario) {
-    document.write(crearSaludo(usuario)+'<br>') 
+function saludarHtml(usuario) {
+    document.write(crearSaludo(usuario)+'<br>')
 }
 
 saludar(userMaria)
 saludar(userPepe)
-saludarHTML(userMaria)
-saludarHTML(userPepe)
-if (1) { }
+saludarHtml(userMaria)
+saludarHtml(userPepe)
 
-if (1) { }
-else { }
 
-if (1) { }
-else if (2) { }
-else if (3) { }
-else { }
+/* if ( 1 ) {}
+else {}
+if ( 1 ) {}
+else if ( 2 ) {}
+else if ( 3 ) {} 
+else {}
+ */
--- 12_js/03_sentencias/js/pares.js
+++ 12_js/03_sentencias/js/pares.js
@@ -1,86 +1,98 @@
-function verificarNumero(numero) {
-    let cadena = ''
-    if (Number(numero)) {
-        if (numero % 2 === 0) {
-            cadena += 'es par'
+'user stric'
+
+/*  Función que comprueba si el argumento es par o impar
+    devolviedo 0, 1, o 
+     -2 si el argumento no es un número
+     -3 si el argumento contene decimales
+*/
+function calcularPar(n) {
+        let r = 0
+        if (isNaN(n/2)) {
+            r = -2
+        } else if (parseInt(n) != parseFloat(n)) {
+            r = -3
         } else {
-            cadena += 'es impar'
+            r = n%2
         }
-
-    } else {
-        cadena += 'Esto no es numero';
+        return r
     }
-    console.log(cadena)
+
+
+/*  Función que comprueba si el argumento es par o impar
+    devolviedo 0, 1, o -1 si el argumento no es un número
+*/
+function calcParEnteros(n) {
+    return isNaN(n/2) ? -1 : parseInt(n)%2
 }
+ function test(n) {
+        console.log(n)
+    } 
 
-verificarNumero(2)
-verificarNumero('a')
-verificarNumero(73)
+/*     test(calcularPar())
+    test(calcularPar('Pepe'))
+    test(calcularPar('12'))
+    test(calcularPar(0))
+    test(calcularPar(12))
+    test(calcularPar(11)) 
+    test(calcularPar(12.4))
+    test(calcularPar(11.7))
+    test(calcPar())
+    test(calcPar('Pepe'))
+    test(calcPar('12'))
+    test(calcPar(0))
+    test(calcPar(12))
+    test(calcPar(11)) 
+*/
 
-/*Funccion que comprueba si el argumento el par o impar
-devolviendo 0, 1, o -1 si el argumento no es un numero*/
-function calcPar(n) {
-    return isNaN(n / 2) ? -1 : n % 2
-}
-
-function calcularPar(n){
-    let r = 0
-    if (isNaN(n/2)) {
-        r = -1
-    }else if (parseInt(n) != parseFloat(n)) {
-        r = -3
-    }else {
-        r = n%2
-    }
-    return r
-}
-
-function mostrarEsPar(n) {
+function mostrarEsPar (n) {
     const aMensajes = [
-        `El numero ${n} es PAR`,
-        `El numero ${n} es IMPAR`,
-        `El dato ${n} no es un numero`,
-        `El numero ${n} es decimal`];
-    //r puede valer 0, 1, -1
+        `El número ${n} es PAR`,
+        `El número ${n} es IMPAR`,
+        `El dato ${n} no es un número`,
+        `El número ${n} es decimal`
+        ]
+    // r puede valer 0, 1, -1    
     let r = calcularPar(n)
-    if (r > 0) { r = -r}
-    
-    //r = (r === -1) ? 2 : r;
-    //si r es -1, lo convierto en 2
-    if (r === -1) { r = 2 }
-    if (r === -2) { r = 3}
+    // si r es -2 o -3, lo convierto en 2 o 3
+    if (r < 0) { r = -r }
+    // muestro la posicion r del array
     console.log(aMensajes[r])
 }
 
-
-/* calcularPar(6)
-mostrarEsPar(111)
-mostrarEsPar('pepe') */
-
-
-function mostrarEsParCasos(n){
-     //r puede valer 0, 1, -2, -3
-    let r = calcularPar(n) 
-    let mensaje 
-    switch (r) {
-        case 0:
-        mensaje =`El numero ${n} es PAR`
-        break
-        case 1:
-        mensaje =`El numero ${n} es IMPAR`
-        break
-        case -2:
-        mensaje =`El dato ${n} no es un numero`
-        break
-        case -3:
-        mensaje =`El numero ${n} es decimal`
-        
-    }
-    console.log(mensaje)
+function mostrarEsParCasos (n) {
+      // r puede valer 0, 1, -2, -3    
+      let r = calcularPar(n)
+      let mensaje
+      switch (r) {
+          case 0 :
+            mensaje =  `El número ${n} es PAR`
+            break
+          case 1 :
+            mensaje = `El número ${n} es IMPAR`
+            break
+          case -2 :
+            mensaje = `El dato ${n} no es un número`
+            break
+          case -3 :
+            mensaje =  `El número ${n} es decimal`
+            break
+      }
+      console.log(mensaje)
 }
+
+
+// Test
+
+/* mostrarEsPar('Pepe')
+mostrarEsPar(12)
+mostrarEsPar(11)
+mostrarEsPar(12.5)
+mostrarEsPar(11.5)
+ */
 
 
 mostrarEsParCasos('Pepe')
 mostrarEsParCasos(12)
-mostrarEsParCasos(111)
+mostrarEsParCasos(11)
 mostrarEsParCasos(12.5)
+mostrarEsParCasos(11.5)
