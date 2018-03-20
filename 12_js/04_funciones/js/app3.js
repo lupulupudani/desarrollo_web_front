f163268aca457fbb8858d5bb564571ed38077abd
Daniel <factordany@yahoo.es>
2018-03-20 20:27:28 +0100

    funciones

--- 12_js/04_funciones/js/app3.js
+++ 12_js/04_funciones/js/app3.js
@@ -1,33 +1,45 @@
-
-
-function calcularCuadrado (a) {
+function calcularCuadrado ( a ) {
     a *= a
-    return a
+    return a 
 }
 
-function calcularCubo (a) {
-    a = a * a * a 
-    return a
+function calcularCubo ( a ) {
+    a = a * a * a
+    return a 
 }
 
-function MostrarCalculo (n, f) {
-    console.log(`El resultado es ${f(n)}`)
-
+function MostrarCalculo(n, f) {
+    console.log(`El resultado es ${f(n)}`)    
 }
 
-let n = 5
-/* MostrarCalculo(n, calcularCuadrado)
+/* 
+MostrarCalculo(n, calcularCuadrado)
 MostrarCalculo(n, calcularCubo)
-MostrarCalculo(n, function(){
+MostrarCalculo(n, function() {
     return n*2
 }) */
 
+let n = 5
 
-setTimeout(function () {
+function mostrarResultado1 () {
     console.log(calcularCubo(n))
-}, 2000)
-
-for (let i = 0; i < 10; i++) {
-    console.log(i)
-    
 }
+
+let mostrarResultado2 = function () {
+    console.log(calcularCubo(n))
+} 
+
+setTimeout( mostrarResultado1 , 1000)
+setTimeout( mostrarResultado2 , 2000)
+setTimeout( function () {
+    console.log(calcularCubo(n))}  , 3000)
+
+for (let i = 0; i < 10000; i++) {
+    console.log(i); 
+}
+
+console.log(typeof mostrarResultado1)
+console.log(typeof mostrarResultado2)
+
+let miFuncion = mostrarResultado2
+console.log(typeof miFuncion)
