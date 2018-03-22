90a6abe4786a0e466bf5f047874c6ae3f5db77e3
Daniel <factordany@yahoo.es>
2018-03-22 11:18:35 +0100

    areglo palindromo

--- 12_js/05_APIS/palindromo.js
+++ 12_js/05_APIS/palindromo.js
@@ -4,7 +4,7 @@ function palindromo(texto) {
  
     let textoOriginal = texto.toLowerCase()
    
-    let letrasEspacios = textoOriginal.split("")
+    let letrasEspacios = textoOriginal.split()
    
     let textoSinEspacios = letrasEspacios.join('')
    
@@ -15,11 +15,13 @@ function palindromo(texto) {
     for(let i = letras.length - 1; i > 0; i--) {
         if(letras[i] == letrasReves[i]) {
             console.log("Esto es un palíndromo")
+            break
         }
         else {
             console.log("No es un palíndromo")
+            break
         }
     }
 }   
 
-palindromo("hola")
+palindromo("121")
