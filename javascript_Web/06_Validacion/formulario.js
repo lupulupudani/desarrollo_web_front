33f85bff5725bb9f3bb56370f8c125a05a99c514
desarrollo_web_front <factordany@yahoo.es>
2018-04-12 11:40:25 +0200

    modificaciones form

--- javascript_Web/06_Validacion/formulario.js
+++ javascript_Web/06_Validacion/formulario.js
@@ -1,13 +1,13 @@
-import {CURSOS}from './cursos.js'
+import { CURSOS } from './cursos.js'
+
 export class Formulario {
     constructor() {
-
         this.datos = {
             nombre: '',
-            apellido: '',
+            apellido: '', 
             nacimiento: '',
             email: '',
-            password: '',
+            passwd: '', 
             datos: '',
             isOk: false,
             isOk2: false,
@@ -15,37 +15,33 @@ export class Formulario {
             curso: {},
             asignaturas: []
         }
-
-        this.accederDom()
+        this.isLeido = false
+        this.accederDom()   
         this.definirManejadores()
     }
 
     accederDom() {
         this.domFormulario = document.querySelector('#form1')
         this.domBtnSaludar = document.querySelector('#btnSaludar')
-        /* this.domBtnEnviar = document.querySelector('#btnEnviar')
-        this.domBtnBorrar = document.querySelector('#btnBorrar') */
         this.domInpNombre = document.querySelector('#nombre')
         this.domInpApellido = document.querySelector('#apellido')
         this.domInpFecha = document.querySelector('#fecha')
         this.domInpEmail = document.querySelector('#email')
-        this.domInpPassword = document.querySelector('#password')        
+        this.domInpPasswd = document.querySelector('#passwd')
         this.domAreaDatos = document.querySelector('#datos')
-        this.domRadioTurno = document.querySelectorAll("[name='turno']")
-        //this.domRadioTurno = document.getElementsByName('turno')
+        this.domRadioTurno = document.querySelectorAll('[name="turno"]') 
+        // this.domRadioTurno = document.getElementsByName(turno)
         this.domCbxIsOk = document.querySelector('#isOk')
         this.domCbxIsOk2 = document.querySelector('#isOk2')
         this.domSelectCurso = document.querySelector('#curso')
-        this.domTopics = document.querySelector('#topics')
-        this.domDivResultados = document.querySelector('#resultado')        
-        this.domFildAcademic = document.querySelector('#acedemic')
+        this.domSelectTopics = document.querySelector('#topics')
+        this.domDivTopics = document.querySelector('#div_topics')
+        this.domDivResultados = document.querySelector('#resultados')
+        this.domFieldAcedemic = document.querySelector('#acedemic')
     }
 
     definirManejadores() {
         this.domFormulario.addEventListener('submit', this.enviar.bind(this))
-        /*this.domBtnSaludar.addEventListener('click', this.saludar.bind(this))
-         this.domBtnEnviar.addEventListener('click', this.enviar.bind(this))
-        this.domBtnBorrar.addEventListener('click', this.borrar.bind(this)) */
         this.domCbxIsOk.addEventListener('change', this.completar.bind(this))
         this.domSelectCurso.addEventListener('change', this.presentarAsignaturas.bind(this))
     }
@@ -56,21 +52,22 @@ export class Formulario {
 
     enviar(ev) {
         console.log(ev)
-        
         this.recogerDatos()
         this.presentarDatos()
-        ev.preventDefault()
-        //this.domFormulario.submit()
+        if (!this.isLeido) {
+            ev.preventDefault()
+        }
     }
-    /* borrar() {
+
+    /* borrar () {
     } */
 
-    completar (){
-        //this.domFildAcademic.disabled = !this.domFildAcademic.disabled
-        this.domFildAcademic.classList.toggle('ocultar')
-        if (this.domFildAcademic.classList.contains('ocultar')){
-            this.domRadioTurno[0].checked =true
-            this.domSelectCurso.selectedIndex = 0
+    completar() {
+        // this.domFieldAcedemic.disabled = !this.domFieldAcedemic.disabled
+        this.domFieldAcedemic.classList.toggle('ocultar')
+        if (this.domFieldAcedemic.classList.contains('ocultar')) {
+            this.domRadioTurno[0].checked = true
+            this.domSelectCurso.selectedIndex=0 
         }
     }
 
@@ -79,62 +76,61 @@ export class Formulario {
         this.datos.apellido = this.domInpApellido.value
         this.datos.nacimiento = new Date(this.domInpFecha.value)
         this.datos.email = this.domInpEmail.value
-        this.datos.password = this.domInpPassword.value
-        this.datos.datos = this.domAreaDatos.value
-        this.datos.turno = this.procesarRadio(this.domRadioTurno)
+        this.datos.passwd =  this.domInpPasswd.value
+        this.datos.datos =  this.domAreaDatos.value
+        this.datos.turno = this.procesarRadio(this.domRadioTurno)  
         this.datos.isOk = this.domCbxIsOk.checked
         this.datos.isOk2 = this.domCbxIsOk2.checked
         this.datos.curso = this.procesarSelect(this.domSelectCurso)
-         
     }
 
     procesarRadio(nodo) {
         let value
-        nodo.forEach((item) => {
+        nodo.forEach( (item ) => {
             if (item.checked) {
                 value = item.value
             }
         })
-        return value //"mañana" "tarde" "noche"
+        return value //"mañana" "tarde"  "noche"
     }
 
-    procesarSelect (nodo){
+    procesarSelect(nodo) {
         let index = nodo.selectedIndex
         return {
-            code: nodo.options[index].value,
+            code: nodo.options[index].value, 
             text: nodo.options[index].textContent
-        }
-        
+        }    
     }
 
-
-    presentarAsignaturas (ev){
+    presentarAsignaturas(ev) {
+        ev.target.firstElementChild.classList.add('ocultar')
         let topics = CURSOS[ev.target.selectedIndex-1].asignaturas
         let HTMLResult = ''
-        topics.forEach ( elem => HTMLResult += `<option>${elem}</option>`)
-        this.domTopics.innerHTML = HTMLResult
+        topics.forEach( elem => HTMLResult +=`<option>${elem}</option>` )
+        this.domDivTopics.classList.remove('ocultar')
+        this.domSelectTopics.innerHTML = HTMLResult
     }
 
-    presentarDatos(){
+    presentarDatos() {
         let resultadoHTML = 
         `<h2>Resultados</h2>
         <ul>
             <li>Nombre: ${this.datos.nombre}</li>
             <li>Apellido: ${this.datos.apellido}</li>
-            <li>Fecha de nacimineto: ${this.datos.nacimiento}</li>
-            <li>Email: ${this.datos.email}</li>
-            <li>Password: ${this.datos.password}</li>
-            <li>Datos: ${this.datos.datos}</li>
+            <li>Fecha nacimiento: ${this.datos.nacimiento}</li>
+            <li>e-Mail: ${this.datos.email}</li>
+            <li>Contraseña: ${this.datos.passwd}</li>
+            <li>Datos extra: ${this.datos.datos}</li>
             <li>Aceptadas condiciones: ${this.datos.isOk ? 'Si' : 'No'}</li>
-            <li>Aceptadas condiciones2: ${this.datos.isOk2 ? 'Si' : 'No'}</li>
+            <li>Aceptadas condiciones 2 ${this.datos.isOk2 ? 'Si' : 'No'}</li>
             <li>Turno: ${this.datos.turno}</li>
             <li>Curso: ${this.datos.curso.text}</li>
         </ul>
         `
-
         this.domDivResultados.innerHTML = resultadoHTML
+        console.dir(this.datos)
+
     }
 
-
-    
+   
 }
--- javascript_Web/06_forms/formulario.js
+++ javascript_Web/06_forms/formulario.js
@@ -1,43 +1,59 @@
+import { listaAsignaturas } from './asignaturas.js'
 
 export class Formulario {
     constructor() {
         this.datos = {
             nombre: '',
             apellido: '',
+            nacimiento: '',
             email: '',
             passwd: '',
+            passwd2: '',
             datos: '',
             isOk: false,
             isOk2: false,
             turno: '',
-            curso: {}
+            curso: {},
+            asignatura: {}
         }
+        this.isLeido = false
         this.accederDom()
         this.definirManejadores()
     }
 
     accederDom() {
+        this.domFormulario = document.querySelector('#form1')
+        //Botones
         this.domBtnSaludar = document.querySelector('#btnSaludar')
-        this.domBtnEnviar = document.querySelector('#btnEnviar')
-        this.domBtnBorrar = document.querySelector('#btnBorrar')
+        //this.domBtnEnviar = document.querySelector('#btnEnviar')
+        //this.domBtnBorrar = document.querySelector('#btnBorrar')
+        //Datos personales
         this.domInpNombre = document.querySelector('#nombre')
         this.domInpApellido = document.querySelector('#apellido')
+        this.domInpFechaNacimiento = document.querySelector('#fecha')
         this.domInpEmail = document.querySelector('#email')
         this.domInpPasswd = document.querySelector('#passwd')
+        this.domInpPasswd2 = document.querySelector('#passwd2')
+        //Otros datos
         this.domAreaDatos = document.querySelector('#datos')
+        //Turnos
         this.domRadioTurno = document.querySelectorAll('[name="turno"]')
-        // this.domRadioTurno = document.getElementsByName(turno)
+        //Conformidad
         this.domCbxIsOk = document.querySelector('#isOk')
         this.domCbxIsOk2 = document.querySelector('#isOk2')
+        //Selección Curso y asignatura
         this.domSelectCurso = document.querySelector('#curso')
+        this.domSelectAsignatura = document.querySelector('#asignatura')
+
         this.domDivResultados = document.querySelector('#resultados')
-        this.domFieldAcedemic = document.querySelector('#acedemic')
+        this.domFieldAcedemic = document.querySelector('#acedemic')//Para ocultar las listas
     }
 
     definirManejadores() {
-        this.domBtnSaludar.addEventListener('click', this.saludar.bind(this))
-        this.domBtnEnviar.addEventListener('click', this.enviar.bind(this))
-        this.domBtnBorrar.addEventListener('click', this.borrar.bind(this))
+        //this.domBtnSaludar.addEventListener('click', this.saludar.bind(this))
+        this.domFormulario.addEventListener('submit', this.enviar.bind(this))
+        //this.domBtnBorrar.addEventListener('click', this.borrar.bind(this))
+        this.domCbxIsOk.addEventListener('change', this.completar.bind(this)) //OJO
         this.domSelectCurso.addEventListener('change', this.cargarAsignaturas.bind(this))
     }
 
@@ -45,34 +61,54 @@ export class Formulario {
         console.log('Hola amigos')
     }
 
+    validarContraseña(){
+        /** 
+         * https://developer.mozilla.org/es/docs/HTML/HTML5/Validacion_de_restricciones
+        */
+        var msg = "";
+        if (this.domInpPasswd.value != this.domInpPasswd2.value) {
+            msg = "Las contraseñas no son iguales"
+        }
+        this.domInpPasswd2.setCustomValidity(msg)
+    }
+
     enviar(ev) {
-        ev.preventDefault()
+        console.log(ev)
+        this.validarContraseña();
         this.recogerDatos()
         this.presentarDatos()
+        //this.domFormulario.submit();
+        if (!this.isLeido) {
+            ev.preventDefault(); //Se salta las validaciones y evita el evento del formulario subtmit
+        }
     }
 
     borrar() {
     }
 
     completar() {
-        this.domFieldAcedemic.disabled = !this.domFieldAcedemic.disabled
+        // this.domFieldAcedemic.disabled = !this.domFieldAcedemic.disabled
         this.domFieldAcedemic.classList.toggle('ocultar')
         if (this.domFieldAcedemic.classList.contains('ocultar')) {
             this.domRadioTurno[0].checked = true
             this.domSelectCurso.selectedIndex = 0
+            this.domSelectAsignatura.selectedIndex = 0
         }
     }
 
     recogerDatos() {
         this.datos.nombre = this.domInpNombre.value
         this.datos.apellido = this.domInpApellido.value
+        this.datos.nacimiento = this.domInpFechaNacimiento.value
         this.datos.email = this.domInpEmail.value
         this.datos.passwd = this.domInpPasswd.value
+        this.datos.passwd2 = this.domInpPasswd2.value
         this.datos.datos = this.domAreaDatos.value
         this.datos.turno = this.procesarRadio(this.domRadioTurno)
         this.datos.isOk = this.domCbxIsOk.checked
         this.datos.isOk2 = this.domCbxIsOk2.checked
         this.datos.curso = this.procesarSelect(this.domSelectCurso)
+        this.datos.asignatura = this.procesarSelect(this.domSelectAsignatura)
     }
 
     procesarRadio(nodo) {
@@ -85,6 +121,7 @@ export class Formulario {
         return value //"mañana" "tarde"  "noche"
     }
 
+    //
     procesarSelect(nodo) {
         let index = nodo.selectedIndex
         return {
@@ -93,49 +130,50 @@ export class Formulario {
         }
     }
 
+    /**
+     * Presentación de datos en el div de resultados, al final de la página del formulario
+     */
     presentarDatos() {
         let resultadoHTML =
             `<h2>Resultados</h2>
-        <ul>
+            <ul>
             <li>Nombre: ${this.datos.nombre}</li>
             <li>Apellido: ${this.datos.apellido}</li>
+            <li>Fecha de Nacimiento: ${this.datos.nacimiento}</li>
             <li>e-Mail: ${this.datos.email}</li>
             <li>Contraseña: ${this.datos.passwd}</li>
+            <li>Contraseña 2: ${this.datos.passwd}</li>
             <li>Datos extra: ${this.datos.datos}</li>
             <li>Aceptadas condiciones: ${this.datos.isOk ? 'Si' : 'No'}</li>
-            <li>Aceptadas condiciones 2 ${this.datos.isOk2 ? 'Si' : 'No'}</li>
+            <li>Aceptadas condiciones 2: ${this.datos.isOk2 ? 'Si' : 'No'}</li>
             <li>Turno: ${this.datos.turno}</li>
             <li>Curso: ${this.datos.curso.text}</li>
-            
-        </ul>
-        `
+            <li>Asignatura: ${this.datos.asignatura.text}</li>
+            </ul>`
         this.domDivResultados.innerHTML = resultadoHTML
-
     }
 
-    cargarAsignaturas() {
-
-        let listaAsignaturas = {
-            front: ["html5", "css3", "Javascript", "JQuery"],
-            web: ["Dreamweaver", "Visual Studio Code", "Bloc Note"],
-            node: ["php", "hibernate", "spring", "Java"],
-            sql: ["sql"]
-        }
-
+    /**
+     * Cargar un SELECT de cursos y su vez, según elección de asignaturas de cada curso
+     */
+    cargarAsignaturas(ev) {
+        // Objeto de Cursos con asignaturas está en un fichero aparte llamado asignaturas.js
+        console.log(ev.target.selectedIndex)
         let cursos = document.getElementById('curso')
         let asignaturas = document.getElementById('asignatura')
         let cursoSeleccionado = cursos.value
 
-
-        asignaturas.innerHTML = '<option value="">Seleccione un Asignatura...</option>'
+        // Se limpian las asignaturas
+        asignaturas.innerHTML = '<option value="">Seleccione una ...</option>'
 
         if (cursoSeleccionado !== '') {
-            
+            // Se seleccionan las asignaturas y se ordenan
             cursoSeleccionado = listaAsignaturas[cursoSeleccionado]
             cursoSeleccionado.sort()
 
-            
-            cursoSeleccionado.forEach(function (asignatura) {
+            // Insertamos las asignaturas
+            //cursoSeleccionado.forEach(function (asignatura) {
+            cursoSeleccionado.forEach(asignatura => {
                 let opcion = document.createElement('option')
                 opcion.value = asignatura
                 opcion.text = asignatura
@@ -144,6 +182,4 @@ export class Formulario {
         }
 
     }
-
 }
-
